619661c53934716fd5fc808e72075b33de2aa253
Dziubynskyi Yuriy <Dziubynskyi@gmail.com>
2017-03-04 17:20:32 +0200

    update test all

--- src/test/java/ru/test/pages/TestAll.java
+++ src/test/java/ru/test/pages/TestAll.java
@@ -2,7 +2,6 @@ package ru.test.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
@@ -34,6 +33,8 @@ public class TestAll extends TestBase {
         loginLiteCart.WaitTillElementIsDisplayed();
         Assert.assertTrue(loginLiteCart.IsOnLiteCartPage());
     }
+
+    //"//*[@id='app-']/a/span[2]"
     @Test
     public void ClickElementsByArray() {
         driver.get("http://localhost/litecart/admin/login.php");
@@ -41,15 +42,31 @@ public class TestAll extends TestBase {
         loginLiteCart.SelectPassword("admin");
         loginLiteCart.PressLoginButton();
         loginLiteCart.WaitTillElementIsDisplayed();
-        List<WebElement> Element = driver.findElements(By.xpath("//*[@id='app-']/a/span[2]"));
-        System.out.println("ElementsSize" + Element.size());
+        List<WebElement> Element = driver.findElements(By.cssSelector("li#app-"));
+        //Создаем лист веб элеменов
+        System.out.println("ElementsSize = " + Element.size());
+        //Узнаем размер листа
         for (int i = 0; i < Element.size(); i++) {
-            Element = driver.findElements(By.xpath("//*[@id='app-']/a/span[2]"));
+            Element = driver.findElements(By.cssSelector("li#app-"));
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-         Element.get(i).click();
-        loginLiteCart.ElementIsPresent();
+            Element.get(i).click();
+            //Нажиммаем ну каждый элемент в листе
+            loginLiteCart.ElementIsPresent();
         }
+    }
 
+    @Test
+    public void CheckStickers() {
+        driver.navigate().to("http://localhost/litecart/en/");
+        loginLiteCart.WaitWebSiteLoaded();
+        List<WebElement> productsList = driver.findElements(By.cssSelector("a.link[title*=Duck]"));//
+        //В цикле проверяем, какое количество стикеров имеет каждый товар
+        for (int i = 0; i < productsList.size(); i++) {
+            if (productsList.get(i).findElements(By.cssSelector("[class^=sticker]")).size() != 1) {
+                //Если количесвто стикеров не равно 1
+            }
+
+        }
     }
 
 }
